7e56173c1ebc89c311bedec5b19231c72af13050
Chase <chase@sillevis.net>
2023-04-05 12:31:31 +0000

    Return index or 404 for standard paths

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var (
 	bind        = flag.String("b", "127.0.0.1:8080", "Bind address")
 	verbose     = flag.Bool("v", false, "Show access log")
 	credentials = flag.String("c", "", "The path to the keyfile. If not present, client will use your default application credentials.")
+	defaultIndex = flag.String("i", "index.html", "The default index file to serve.")
 )
 
 var (
@@ -28,8 +29,29 @@ var (
 
 func handleError(w http.ResponseWriter, err error) {
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleNotFound(w http.ResponseWriter, r *http.Request, object string, err error) {
+	if err != nil {
 		if err == storage.ErrObjectNotExist {
-			http.Error(w, err.Error(), http.StatusNotFound)
+			params := mux.Vars(r)
+			if *verbose {
+				log.Printf("object %s", object)
+			}
+			if strings.HasSuffix(object, *defaultIndex) {
+				if *verbose {
+					log.Printf("fetching 404")
+				}
+				fetchObject(w, r, params["bucket"], "404.html")
+			} else {
+				if *verbose {
+					log.Printf("fetching default index %s", fmt.Sprintf("%s/%s", object, *defaultIndex))
+				}
+				fetchObject(w, r, params["bucket"], fmt.Sprintf("%s/%s", object, *defaultIndex))
+			}
 		} else {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -106,11 +128,31 @@ func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
 func proxy(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
+	if *verbose {
+		log.Printf("proxying %s, object %s", params["bucket"], params["object"])
+	}
+	if params["object"] == "" {
+		params["object"] = *defaultIndex
+	}
+	if strings.HasSuffix(params["object"], "/") {
+		params["object"] += *defaultIndex
+	}
 	gzipAcceptable := clientAcceptsGzip(r)
 	obj := client.Bucket(params["bucket"]).Object(params["object"]).ReadCompressed(gzipAcceptable)
 	attr, err := obj.Attrs(ctx)
 	if err != nil {
-		handleError(w, err)
+		if err == storage.ErrObjectNotExist {
+			if *verbose {
+				log.Printf("object does not exist %s", object)
+			}
+			if object == "404.html" {
+				http.Error(w, err.Error(), http.StatusNotFound)
+			} else {
+				handleNotFound(w, r, object, err)
+			}
+		} else {
+			handleError(w, err)
+		}
 		return
 	}
 	if lastStrs, ok := r.Header["If-Modified-Since"]; ok && len(lastStrs) > 0 {
